14df6ba29cfc195ced6c4a34046cc264ef6c0510
rafciu10 <rafciu10@gmail.com>
2020-04-16 08:43:17 +0200

    Generating random cities

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-print('Hello world!')
-print('Hello back!')
+import random
+import math
+
+def randomCityGenerator():             # creates 5 random (x,y) coordinates and returns two lists, one
+    randomXlist = []                   # with x coordinate and one with y coordinates
+    randomYlist = []
+    for i in range (1, 6):
+        xi = random.randrange(1, 250)
+        randomXlist.append(xi)
+        yi = random.randrange(1, 250)
+        randomYlist.append(yi)
+        print ('x', i, ':', xi, sep='')
+        print ('y', i, ':', yi, sep='')
+        print ('\n')
+    print(randomXlist)
+    print(randomYlist)
+
+def calculateDistance(x1, y1, x2, y2):     #calculates distance between two given (x,y) coordinates
+    dist = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return dist
+
+
+#def main():
+randomCityGenerator()
+
+
